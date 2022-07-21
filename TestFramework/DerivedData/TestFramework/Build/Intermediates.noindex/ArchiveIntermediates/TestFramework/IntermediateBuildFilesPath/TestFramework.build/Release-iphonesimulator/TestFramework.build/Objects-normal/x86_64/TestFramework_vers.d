67a66565f169d39bcbcf3c1175af206c9d8a57ec
varaarul <varaarul@umich.edu>
2022-07-21 12:24:33 -0700

    added framework

---
+++ TestFramework/DerivedData/TestFramework/Build/Intermediates.noindex/ArchiveIntermediates/TestFramework/IntermediateBuildFilesPath/TestFramework.build/Release-iphonesimulator/TestFramework.build/Objects-normal/x86_64/TestFramework_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/varaarul/onymos2022/TestFramework/DerivedData/TestFramework/Build/Intermediates.noindex/ArchiveIntermediates/TestFramework/IntermediateBuildFilesPath/TestFramework.build/Release-iphonesimulator/TestFramework.build/DerivedSources/TestFramework_vers.c
---
+++ TestFramework/TestFramework/Printer.swift
@@ -0,0 +1,14 @@
+//
+//  Printer.swift
+//  TestFramework
+//
+//  Created by Vara Arul on 7/21/22.
+//
+
+import Foundation
+
+public struct Printer {
+    public static func helloMessage() {
+        print("Hello all testing testing 123")
+    }
+}
